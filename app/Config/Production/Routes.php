8a6c634c3c47f2854c17cb4734455b63f585ed75
virgo888 <48339429+vindypratama@users.noreply.github.com>
2020-08-22 18:38:55 +0700

    Menambahkan routes untuk production

--- app/Config/Production/Routes.php
+++ app/Config/Production/Routes.php
@@ -2,8 +2,4 @@
 /**
  * @var  \CodeIgniter\Router\RouteCollection $routes
  */
-
-$routes->resource('api/users');
-$routes->get('api/users/(:segment)',      'Users::show/$1');
-
-// $routes->presenter('api/users');
+$routes->resource('api/users', ['controller' => 'Api\Users']);
